0ace53783f8f39895afe1af3c338c7d226b7ca0c
devthanekar <devthanekar62@gmail.com>
2026-06-04 11:29:02 +0530

    any instruction you want

---
+++ Day9_CRT/9.1_prime number.c
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+bool isPrime(int n) {
+    if (n < 2)
+    return false;
+    for (int i = 2; i * i <= n; i++) {
+    if (n % i == 0)
+    return false;
+    }
+    return true;
+}
+
+void generatePrimes(int start, int end) {
+    for (int i = start; i <= end; i++) {
+    if (isPrime(i))
+    cout << i << " ";
+    }
+}
+int main() {
+    int start = 10, end = 50;
+    cout << "Prime numbers in " << start << " and " << end << ": ";
+    generatePrimes(start, end);
+    return 0;
+}
---
+++ Day9_CRT/9.2_queue_using_array.cpp
@@ -0,0 +1,135 @@
+#include<iostream>
+using namespace std;
+
+class Queue{
+private://decl ration of data member
+    int *arr;
+    int front;
+    int rear;
+    int capacity;
+
+
+public://initialization
+   Queue(int size)//constructor
+   {
+       capacity=size;//size of queue
+       arr=new int[capacity];//capacit or size of array or queue
+       front=0;//initial state
+       rear=-1;//initial state
+   }
+   bool isEmpty()
+   {
+       return rear < front;
+   }
+   bool isFull()
+   {
+       return rear==capacity-1;
+   }
+   void Enqueue(int value )
+   {
+       if(isFull()){
+        cout<<"Queue overflow! cannot insert"<<value<<endl;
+        return ;
+       }
+       arr[++rear]=value;
+       cout<<value<<"insert into queue"<<endl;
+   }
+   void Display(){
+       if(isEmpty()){
+        cout<<"Queue is empty"<<endl;
+        return ;
+
+       }
+       for(int i=front;i<rear;i++){
+        cout<<arr[i]<<" ";
+       }
+   }
+   void dequeue(){
+   if(isEmpty())
+   {
+   cout<<"Queue Underfloaw! Queue is empty"<<endl;
+   return ;
+   }
+   cout<<arr[front]<<" removed from queue"<<endl;
+   front++;
+
+    }
+    void frontElement(){
+    if(isEmpty()){
+        cout<<"Queue is empty"<<endl;
+        return ;
+
+    }
+    cout<<"Front value="<<arr[front];
+    }
+    void delet()
+{
+    if(arr == NULL)
+    {
+        cout << "Queue already deleted" << endl;
+        return;
+    }
+
+    delete[] arr;
+    arr = NULL;
+    front=0;
+    rear = -1;
+    capacity = 0;
+
+    cout << "Queue is deleted" << endl;
+}
+    };
+
+
+int main(){
+    int s;
+    cout<<"Enter size of Queue"<<endl;
+    cin>>s;
+    Queue obj(s);
+    cout<<"Queue has created"<<endl;
+    while(true)
+{
+    int choice;
+    cout<<endl;
+    cout<<"1. Enqueu"<<endl;
+    cout<<"2. Display"<<endl;
+    cout<<"3. Dequeue"<<endl;
+    cout<<"4. Peek"<<endl;
+    cout<<"5. Delete queue"<<endl;
+    cout<<"6. Exit"<<endl;
+    cout<<"Enter your choice"<<endl;
+    cin>>choice;
+    if(choice == 1)
+    {
+        int val;
+        cout<<"Enter the element for insert"<<endl;
+        cin>>val;
+       obj.Enqueue(val);
+    }else if(choice ==2)
+    {
+    obj.Display();
+    }
+    else if(choice ==3)
+    {
+    obj.dequeue();
+    }
+     else if(choice ==4)
+    {
+    obj.frontElement();
+    }
+    else if(choice ==4)
+    {
+    obj.delet();
+    }
+    else{
+        break;
+    }
+
+
+
+
+
+    }
+
+}
+
